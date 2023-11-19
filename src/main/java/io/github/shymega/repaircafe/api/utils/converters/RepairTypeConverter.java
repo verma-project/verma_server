c748580efdf9d804954e1683dc9478dde101edf5
Dom Rodriguez <shymega@shymega.org.uk>
2023-11-19 22:40:01 +0000

    feat(Repair): Add `RepairType` enum based on NHRC Ti.to
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/main/java/io/github/shymega/repaircafe/api/utils/converters/RepairTypeConverter.java
@@ -0,0 +1,23 @@
+package io.github.shymega.repaircafe.api.utils.converters;
+
+import io.github.shymega.repaircafe.api.enums.RepairTypeEnum;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Converter;
+
+@Converter
+public class RepairTypeConverter implements AttributeConverter<RepairTypeEnum, String> {
+    @Override
+    public String convertToDatabaseColumn(RepairTypeEnum repairTypeEnum) {
+        if (repairTypeEnum == null) throw new IllegalArgumentException("DB error. Enum variant is not populated.");
+
+        return repairTypeEnum.toString();
+    }
+
+    @Override
+    public RepairTypeEnum convertToEntityAttribute(String s) {
+        if (s == null || s.isEmpty()) throw new IllegalArgumentException("DB error. Input string is not populated.");
+
+        return RepairTypeEnum.valueOf(s);
+    }
+}
