ee79487eff5bdc0157ab52eaefb3e41480bece8f
Dom Rodriguez <shymega@shymega.org.uk>
2023-11-19 22:40:01 +0000

    feat(Repair): Add `RepairType` enum based on NHRC Ti.to
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/main/java/io/github/shymega/repaircafe/api/enums/RepairTypeEnum.java
@@ -0,0 +1,13 @@
+package io.github.shymega.repaircafe.api.enums;
+
+public enum RepairTypeEnum {
+    COMPUTER,
+    SMALL_ELECTRICAL,
+    CLOTHES,
+    FURNITURE,
+    GARDEN_TOOL,
+    TOY,
+    BICYCLE,
+    JEWELLERY,
+    OTHER,
+}
