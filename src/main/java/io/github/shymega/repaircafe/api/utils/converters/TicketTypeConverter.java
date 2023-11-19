bef52069ac27c02b76ba09d4a5783c40b0cb99bc
Dom Rodriguez <shymega@shymega.org.uk>
2023-11-19 22:39:40 +0000

    fix(TicketTypeConverter): Return useful error on Converter
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/io/github/shymega/repaircafe/api/utils/converters/TicketTypeConverter.java
+++ src/main/java/io/github/shymega/repaircafe/api/utils/converters/TicketTypeConverter.java
@@ -8,14 +8,14 @@ import jakarta.persistence.Converter;
 public class TicketTypeConverter implements AttributeConverter<TicketTypeEnum, String> {
     @Override
     public String convertToDatabaseColumn(TicketTypeEnum ticketTypeEnum) {
-        if (ticketTypeEnum == null) throw new IllegalArgumentException("error"); // FIXME.
+        if (ticketTypeEnum == null) throw new IllegalArgumentException("DB error. Enum variant is not populated.");
 
         return ticketTypeEnum.toString();
     }
 
     @Override
     public TicketTypeEnum convertToEntityAttribute(String s) {
-        if (s == null || s.isEmpty()) throw new IllegalArgumentException("error"); // FIXME.
+        if (s == null || s.isEmpty()) throw new IllegalArgumentException("DB error. Input string is not populated.");
 
         return TicketTypeEnum.valueOf(s);
     }
