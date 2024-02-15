a68f9a49e9bc7ff47336f6df2df0674a524ef18e
Dom Rodriguez <shymega@shymega.org.uk>
2024-02-15 21:59:10 +0000

    feat(RepairRepository): Add new method
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/org/deraproject/apps/server/db/repositories/RepairRepository.java
+++ src/main/java/org/deraproject/apps/server/db/repositories/RepairRepository.java
@@ -1,9 +1,12 @@
 package org.deraproject.apps.server.db.repositories;
 
 import org.deraproject.apps.server.db.entities.Repair;
+import org.deraproject.apps.server.enums.RepairTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RepairRepository extends JpaRepository<Repair, UUID> {
+    List<Repair> findRepairsByRepairType(RepairTypeEnum repairType);
 }
