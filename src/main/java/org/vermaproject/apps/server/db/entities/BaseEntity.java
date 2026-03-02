cd16fc0c97a288370d4fd9e4503f21641eb83df8
Dom Rodriguez <shymega@shymega.org.uk>
2026-03-02 19:35:37 +0000

    fix: Remove super Id from BaseEntity

--- src/main/java/org/vermaproject/apps/server/db/entities/BaseEntity.java
+++ src/main/java/org/vermaproject/apps/server/db/entities/BaseEntity.java
@@ -4,21 +4,14 @@
 
 package org.vermaproject.apps.server.db.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Version;
 import lombok.Getter;
 
-import java.util.UUID;
 
 @MappedSuperclass
 @Getter
 public class BaseEntity {
-    @Id
-    @GeneratedValue
-    private UUID id;
-
     @Version
     private Integer version;
 }
