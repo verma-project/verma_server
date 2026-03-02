c8fe8cfed5ce1a2ba375d07f0f674d395b5f7edc
Dom Rodriguez <shymega@shymega.org.uk>
2026-03-02 18:01:05 +0000

    refactor: Update and simplfy Flake and lockfile

--- flake.nix
+++ flake.nix
@@ -3,46 +3,50 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     mvn2nix.url = "github:fzakaria/mvn2nix";
-    systems.url = "github:nix-systems/default";
     devenv = {
       url = "github:cachix/devenv";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
   };
 
   outputs = inputs: let
-    inherit (inputs) self nixpkgs utils devenv;
-    genPkgs = system:
-      import nixpkgs {
-        inherit system;
-        overlays = with inputs; [
-          mvn2nix.overlay
-          (final: _: rec {
-            verma_server = final.callPackage ./nix/package.nix {inherit self;};
-          })
-        ];
-      };
-  in
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = genPkgs system;
+    inherit (inputs) self nixpkgs devenv;
+    forEachSystem = let
+      genPkgs = system:
+        import nixpkgs {
+          inherit system;
+          overlays = with inputs; [
+            mvn2nix.overlay
+            (final: _: rec {
+              verma_server = final.callPackage ./nix/package.nix {inherit self;};
+            })
+          ];
+        };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      inherit (nixpkgs.lib) genAttrs;
+    in
+      f: genAttrs systems (system: f (genPkgs system));
+  in {
+    packages = forEachSystem (pkgs: let
       inherit (pkgs) verma_server;
     in {
-      packages = {
-        inherit verma_server;
-        oci = pkgs.callPackage ./nix/oci.nix {inherit self verma_server;};
-        default = self.packages.${system}.verma_server;
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      };
+      inherit verma_server;
+      oci = pkgs.callPackage ./nix/oci.nix {inherit self verma_server;};
+      default = self.packages.${pkgs.stdenv.hostPlatform.system}.verma_server;
+      devenv-up = self.devShells.${pkgs.stdenv.hostPlatform.system}.default.config.procfileScript;
+    });
 
-      devShells.default = devenv.lib.mkShell {
+    devShells.default = forEachSystem (pkgs:
+      devenv.lib.mkShell {
         inherit inputs pkgs;
         modules = pkgs.lib.singleton ./devenv.nix;
-      };
-    });
+      });
+  };
 }
