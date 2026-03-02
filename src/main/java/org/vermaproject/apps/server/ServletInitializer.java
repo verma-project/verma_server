e26ad39f59a81426820df97a2ac31eff3c57ebc4
Dom Rodriguez <shymega@shymega.org.uk>
2026-03-02 19:33:42 +0000

    feat: init ServletInitializer for Tomcat servers

---
+++ src/main/java/org/vermaproject/apps/server/ServletInitializer.java
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2023-2025 Dom Rodriguez (shymega) <shymega@shymega.org.uk>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package org.vermaproject.apps.server;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(Application.class);
+    }
+}
