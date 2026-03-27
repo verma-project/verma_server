a7b7c7f6b7e2296413980137c666275122b562d6
Dom Rodriguez <shymega@shymega.org.uk>
2026-03-27 18:29:23 +0000

    feat: Simplify nixConfig in Flake

--- flake.nix
+++ flake.nix
@@ -10,6 +10,17 @@
     };
   };
 
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://devenv.cachix.org"
+    ];
+    public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+  };
+
   outputs = inputs: let
     inherit (inputs) self nixpkgs devenv;
     forEachSystem = let
